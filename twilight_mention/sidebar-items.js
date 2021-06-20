ffaa004007b8817f4a49fde3fb5986fe56570965
vivian <vivian@users.noreply.github.com>
2021-06-20 14:33:35 +0000

    deploy: bd48da4c77f6809290d614dff1259ef8cc1c8a39

--- twilight_mention/sidebar-items.js
+++ twilight_mention/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["fmt","Formatters for creating mentions."],["parse","Parse mentions out of strings."]]});
+initSidebarItems({"mod":[["fmt","Formatters for creating mentions."],["parse","Parse mentions out of strings."],["timestamp","Timestamps with the ability to be formatted in clients based on the client’s local timezone and locale."]]});
