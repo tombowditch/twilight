056cca837a14f4df9195480e49e2a1590a6f872a
7596ff <7596ff@users.noreply.github.com>
2021-07-01 14:17:48 +0000

    deploy: e417ddf05c2b3c40770c788320101f6d3b72b7bb

--- twilight_cache_inmemory/model/sidebar-items.js
+++ twilight_cache_inmemory/model/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["CachedEmoji",""],["CachedGuild",""],["CachedMember",""],["CachedMessage",""],["CachedPresence",""],["CachedVoiceState",""]]});
+initSidebarItems({"struct":[["CachedEmoji","Represents a cached `Emoji`."],["CachedGuild","Represents a cached `Guild`."],["CachedMember","Represents a cached `Member`."],["CachedMessage","Represents a cached `Message`."],["CachedPresence","Represents a cached `Presence`."],["CachedVoiceState","Represents a cached `VoiceState`."]]});
